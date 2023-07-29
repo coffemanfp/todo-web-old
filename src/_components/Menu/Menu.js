fc549424264028457a2c2fb8b40dfc02a19f1e2c
Franklin Peñaranda <coffemanfp@gmail.com>
2023-07-29 18:47:51 -0500

    Adding Menu component. Only layout.

---
+++ src/_components/Menu/Menu.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Menu.scss'
+
+export default function Menu() {
+    return (
+        <nav className="menu">
+            <a href="#" className="menu__item"><i className="menu__item-icon bx bx-sun"></i> My Day</a>
+            <a href="#" className="menu__item"><i className="menu__item-icon bx bx-star"></i> Important</a>
+            <a href="#" className="menu__item"><i className="menu__item-icon bx bx-calendar"></i> Planned</a>
+            <a href="#" className="menu__item"><i className="menu__item-icon bx bx-stopwatch"></i> Expired soon</a>
+            <a href="#" className="menu__item"><i className="menu__item-icon bx bx-grid-small"></i> Expired soon</a>
+            <hr className="menu__separator" />
+            <form className="custom-list-adder">
+                <i className="custom-list-adder__icon bx bx-plus"></i>
+                <input type="text" className="custom-list-adder__input" placeholder='New list' />
+            </form>
+        </nav>
+    )
+}
