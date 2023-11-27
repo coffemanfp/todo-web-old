5b83eb0e9f2fe4a85e2e980b581c92ab78b6496f
Franklin Peñaranda <coffemanfp@gmail.com>
2023-11-27 09:17:45 -0500

    Fix: renamed tasks server endpoints

--- src/_components/TaskPanel/TaskPanel.js
+++ src/_components/TaskPanel/TaskPanel.js
@@ -19,7 +19,7 @@ export default function TaskPanel() {
     const navigate = useNavigate()
     const onSubmit = task => {
         deleteFieldsIfEmpty(task, ["due_date", "reminder", "repeat"])
-            if (taskStatus === 'idle' || taskStatus === 'completed') {
+        if (taskStatus === 'idle' || taskStatus === 'completed') {
             dispatch(taskActions.update(task))
         }
     }
@@ -32,7 +32,7 @@ export default function TaskPanel() {
         const fetchData = async () => {
             setGetOneTaskStatus('loading')
 
-            await fetchWrapper.get(createURL('task/' + taskId))
+            await fetchWrapper.get(createURL('tasks/' + taskId))
                 .then(task => {
                     setTask(task)
                 })
